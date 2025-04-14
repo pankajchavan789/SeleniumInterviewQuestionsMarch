598efdb162f8e52aecb45853f3e29f0c505c7344
pankajchavan789 <pankajchavan4002@gmail.com>
2025-04-14 12:20:56 +0530

    circleshape commit

---
+++ src/test/java/Patterns/circleshape_pattern.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class circleshape_pattern {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int row=1;row<=n;row++) {
+			for(int col=1;col<=row;col++) {
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+           
+	}
+
+}
